0fcc0e5ebefb31b52f6491e0659728c5e4e966b2
Sundara Kumar Padmanabhan <clicksuku@gmail.com>
2024-04-15 06:28:46 +0530

    Added Terraform files and cleaned up steps

---
+++ local/terraform/variables.tf
@@ -0,0 +1,45 @@
+
+
+variable "project" {
+  description = "Project"
+  default     = "skpdezfinalproject1"
+}
+
+variable "location" {
+  description = "Project Location"
+  #Update the below to your desired location
+  default = "US"
+}
+
+variable "region" {
+  description = "Region"
+  #Update the below to your desired region
+  default = "us-west4"
+}
+
+variable "zone" {
+  description = "Zone"
+  #Update the below to your desired region
+  default = "us-west4-b"
+}
+
+variable "gcs_bucket_name" {
+  description = "OLIST Marketing and Customer"
+  #Update the below to a unique bucket name
+  default = "skp-olist-gcp"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
+
+variable "dataset_id" {
+  description = "Dataset ID"
+  default     = "olist"
+}
+
+variable "gcpkey" {
+  sensitive   = true
+  description = "Google Cloud service account credentials"
+}
