dd13ef426c55e72ab9c84e5786903e7fd0c6ae90
Sundara Kumar Padmanabhan <clicksuku@gmail.com>
2024-04-17 05:34:07 +0530

    Update gcp_bucket.tf removing projectID

--- local/terraform/gcp_bucket.tf
+++ local/terraform/gcp_bucket.tf
@@ -2,7 +2,6 @@ resource "google_storage_bucket" "static" {
   name          = var.gcs_bucket_name
   location      = "US"
   storage_class = var.gcs_storage_class
-  project = var.project
 
   uniform_bucket_level_access = true
   public_access_prevention    = "enforced"
